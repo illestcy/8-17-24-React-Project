b61a9123ae48c739376198210a152852a5df0426
illestcy <cyrjmishe@gmail.com>
2024-08-17 11:57:41 +0800

    MapFinal

--- HERO-CARD/src/components/Card.jsx
+++ HERO-CARD/src/components/Card.jsx
@@ -5,11 +5,25 @@ import tb4 from "./jpgs/phantom_assassin.png"
 
 
 
+const cardarray = [{title:"TerrorBlade",description:"FractalHorns",imageUrl:tb},
+    {title:"Morphling",description:"BladeOfTears",imageUrl:tb2},
+    {title:"Weaver",description:"Desolator",imageUrl:tb3},
+    {title:"PA",description:"Paradoxical",imageUrl:tb4}] 
+
+
+
 export default function Card (props) {
     return (
         <>
         <section className="flex space-x-2 justify-center items-center w-screen h-screen bg-neutral-900">
-            <div className="hover:bg-black p-5 bg-neutral-700 text-white rounded-lg">
+            {cardarray?.map((card,index) => (
+                <div className="hover:bg-black p-5 bg-neutral-700 text-white rounded-lg">
+                <img src={card.imageUrl} className="w-20 h-20" />
+                <h1>{card.title}</h1>
+                <p>{card.description}</p>              
+                </div>
+            ))}
+            {/* <div className="hover:bg-black p-5 bg-neutral-700 text-white rounded-lg">
             <img src={tb} className="w-20 h-20" />
             <h1>Terrorblade</h1>
             <p>{props.description}</p>
@@ -28,7 +42,7 @@ export default function Card (props) {
             <img src={tb4} className="w-20 h-20" />
             <h1>PA</h1>
             <p>{props.description}</p>
-            </div>
+            </div> */}
         </section>
         
         
